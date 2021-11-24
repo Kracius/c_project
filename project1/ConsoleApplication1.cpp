d9c9d813a6fca658fe610bac58b8e6fcd7448c8d
Kracius <89945182+Kracius@users.noreply.github.com>
2021-11-24 19:50:51 +0300

    Update ConsoleApplication1.cpp

--- project1/ConsoleApplication1.cpp
+++ project1/ConsoleApplication1.cpp
@@ -1,36 +1,166 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
-double sum(double num1, double num2, char operation) {
-
-    if (operation == '/' && num2 == 0) return 0;
-
-    switch (operation) {
-    case '+': return num1 + num2;
-    case '-': return num1 - num2;
-    case '*': return num1 * num2;
-    case '/': return num1 / num2;
-    default: return -1;
-    }
-}
-
 int main() {
-    setlocale(0, "");
-    cout << "[+] первое число: ";
-    int num1;
-    cin >> num1;
-
-    cout << "[+] второе число: ";
-    int num2;
-    cin >> num2;
-
-    cout << "[+] действие: ";
-    int operation;
-    cin >> operation;
-
-    cout << "[+] Результат: " << sum(num1, num2, operation) << endl;
-
-    int _; cin >> _;
+    int int1 = 100;
+    int int2 = 40;
+    int int3 = 40;
+    int int4 = 50;
+    int int5 = 0;
+    int int6 = 60;
+    int int7 = 30;
+    int int8 = 70;
+    int int9 = 01;
+    int int10 = 0;
+    //
+    short short0 = 20;
+    short short1 = 50;
+    short short2 = 20;
+    short short3 = 0;
+    short short4 = 10;
+    short short5 = 0;
+    short short6 = 02;
+    short short7 = 00;
+    short short8 = 80;
+    short short9 = 30;
+    //
+    long long0 = 10;
+    long long1 = 2;
+    long long2 = 023232;
+    long long3 = 11;
+    long long4 = 20;
+    long long5 = 30;
+    long long6 = 8;
+    long long7 = 12;
+    long long8 = 31;
+    long long9 = 15;
+    //
+    float float0 = 0.0;
+    float float1 = 60.0;
+    float float2 = 0.0;
+    float float3 = 0.0;
+    float float4 = 60.0;
+    float float5 = 40.0;
+    float float6 = 50.0;
+    float float7 = 0.0;
+    float float8 = 0.0;
+    float float9 = 230.0;
+    //
+    double double0 = 0.0;
+    double double1 = 30.0;
+    double double2 = 0.0;
+    double double3 = 0.0;
+    double double4 = 40.0;
+    double double5 = 0.0;
+    double double6 = 03.0;
+    double double7 = 0.0;
+    double double8 = 60.0;
+    double double9 = 40.0;
+    //
+    char char0 = 'a';
+    char char1 = 'b';
+    char char2 = 'c';
+    char char3 = 'd';
+    char char4 = 'e';
+    char char5 = 'f';
+    char char6 = 'g';
+    char char7 = 'h';
+    char char8 = 'i';
+    char char9 = 'j';
+    //
+    bool bool0 = true;
+    bool bool1 = false;
+    bool bool2 = false;
+    bool bool3 = false;
+    bool bool4 = true;
+    bool bool5 = false;
+    bool bool6 = true;
+    bool bool7 = false;
+    bool bool8 = true;
+    bool bool9 = false;
+    //
+    cout << "int1 = " << int1 << " | Размер: " << sizeof(int1) << " байт" << endl;
+    cout << "int2 = " << int2 << " | Размер: " << sizeof(int2) << " байт" << endl;
+    cout << "int3 = " << int3 << " | Размер: " << sizeof(int3) << " байт" << endl;
+    cout << "int4 = " << int4 << " | Размер: " << sizeof(int4) << " байт" << endl;
+    cout << "int5 = " << int5 << " | Размер: " << sizeof(int5) << " байт" << endl;
+    cout << "int6 = " << int6 << " | Размер: " << sizeof(int6) << " байт" << endl;
+    cout << "int7 = " << int7 << " | Размер: " << sizeof(int7) << " байт" << endl;
+    cout << "int8 = " << int8 << " | Размер: " << sizeof(int8) << " байт" << endl;
+    cout << "int9 = " << int9 << " | Размер: " << sizeof(int9) << " байт" << endl;
+    cout << "int10 = " << int10 << " | Размер: " << sizeof(int1) << " байт" << endl;
+    //
+    cout << endl;
+    cout << "short0 = " << short0 << " | Размер: " << sizeof(short0) << " байт" << endl;
+    cout << "short1 = " << short1 << " | Размер: " << sizeof(short1) << " байт" << endl;
+    cout << "short2 = " << short2 << " | Размер: " << sizeof(short2) << " байт" << endl;
+    cout << "short3 = " << short3 << " | Размер: " << sizeof(short3) << " байт" << endl;
+    cout << "short4 = " << short4 << " | Размер: " << sizeof(short4) << " байт" << endl;
+    cout << "short5 = " << short5 << " | Размер: " << sizeof(short5) << " байт" << endl;
+    cout << "short6 = " << short6 << " | Размер: " << sizeof(short6) << " байт" << endl;
+    cout << "short7 = " << short7 << " | Размер: " << sizeof(short7) << " байт" << endl;
+    cout << "short8 = " << short8 << " | Размер: " << sizeof(short8) << " байт" << endl;
+    cout << "short9 = " << short9 << " | Размер: " << sizeof(short9) << " байт" << endl;
+    //
+    cout << endl;
+    cout << "long0 = " << long0 << " | Размер: " << sizeof(long0) << " байт" << endl;
+    cout << "long1 = " << long1 << " | Размер: " << sizeof(long1) << " байт" << endl;
+    cout << "long2 = " << long2 << " | Размер: " << sizeof(long2) << " байт" << endl;
+    cout << "long3 = " << long3 << " | Размер: " << sizeof(long3) << " байт" << endl;
+    cout << "long4 = " << long4 << " | Размер: " << sizeof(long4) << " байт" << endl;
+    cout << "long5 = " << long5 << " | Размер: " << sizeof(long5) << " байт" << endl;
+    cout << "long6 = " << long6 << " | Размер: " << sizeof(long6) << " байт" << endl;
+    cout << "long7 = " << long7 << " | Размер: " << sizeof(long7) << " байт" << endl;
+    cout << "long8 = " << long8 << " | Размер: " << sizeof(long8) << " байт" << endl;
+    cout << "long9 = " << long9 << " | Размер: " << sizeof(long9) << " байт" << endl;
+    //
+    cout << endl;
+    cout << "float0 = " << float0 << " | Размер: " << sizeof(float0) << " байт" << endl;
+    cout << "float1 = " << float1 << " | Размер: " << sizeof(float1) << " байт" << endl;
+    cout << "float2 = " << float2 << " | Размер: " << sizeof(float2) << " байт" << endl;
+    cout << "float3 = " << float3 << " | Размер: " << sizeof(float3) << " байт" << endl;
+    cout << "float4 = " << float4 << " | Размер: " << sizeof(float4) << " байт" << endl;
+    cout << "float5 = " << float5 << " | Размер: " << sizeof(float5) << " байт" << endl;
+    cout << "float6 = " << float6 << " | Размер: " << sizeof(float6) << " байт" << endl;
+    cout << "float7 = " << float7 << " | Размер: " << sizeof(float7) << " байт" << endl;
+    cout << "float8 = " << float8 << " | Размер: " << sizeof(float8) << " байт" << endl;
+    cout << "float9 = " << float9 << " | Размер: " << sizeof(float9) << " байт" << endl;
+    //
+    cout << endl;
+    cout << "double0 = " << double0 << " | Размер: " << sizeof(double0) << " байт" << endl;
+    cout << "double1 = " << double1 << " | Размер: " << sizeof(double1) << " байт" << endl;
+    cout << "double2 = " << double2 << " | Размер: " << sizeof(double2) << " байт" << endl;
+    cout << "double3 = " << double3 << " | Размер: " << sizeof(double3) << " байт" << endl;
+    cout << "double4 = " << double4 << " | Размер: " << sizeof(double4) << " байт" << endl;
+    cout << "double5 = " << double5 << " | Размер: " << sizeof(double5) << " байт" << endl;
+    cout << "double6 = " << double6 << " | Размер: " << sizeof(double6) << " байт" << endl;
+    cout << "double7 = " << double7 << " | Размер: " << sizeof(double7) << " байт" << endl;
+    cout << "double8 = " << double8 << " | Размер: " << sizeof(double8) << " байт" << endl;
+    cout << "double9 = " << double9 << " | Размер: " << sizeof(double9) << " байт" << endl;
+    //
+    cout << endl;
+    cout << "char0 = " << char0 << " | Размер: " << sizeof(char0) << " байт" << endl;
+    cout << "char1 = " << char1 << " | Размер: " << sizeof(char1) << " байт" << endl;
+    cout << "char2 = " << char2 << " | Размер: " << sizeof(char2) << " байт" << endl;
+    cout << "char3 = " << char3 << " | Размер: " << sizeof(char3) << " байт" << endl;
+    cout << "char4 = " << char4 << " | Размер: " << sizeof(char4) << " байт" << endl;
+    cout << "char5 = " << char5 << " | Размер: " << sizeof(char5) << " байт" << endl;
+    cout << "char6 = " << char6 << " | Размер: " << sizeof(char6) << " байт" << endl;
+    cout << "char7 = " << char7 << " | Размер: " << sizeof(char7) << " байт" << endl;
+    cout << "char8 = " << char8 << " | Размер: " << sizeof(char8) << " байт" << endl;
+    cout << "char9 = " << char9 << " | Размер: " << sizeof(char9) << " байт" << endl;
+    //
+    cout << endl;
+    cout << "bool0 = " << bool0 << " | Размер: " << sizeof(bool0) << " байт" << endl;
+    cout << "bool1 = " << bool1 << " | Размер: " << sizeof(bool1) << " байт" << endl;
+    cout << "bool2 = " << bool2 << " | Размер: " << sizeof(bool2) << " байт" << endl;
+    cout << "bool3 = " << bool3 << " | Размер: " << sizeof(bool3) << " байт" << endl;
+    cout << "bool4 = " << bool4 << " | Размер: " << sizeof(bool4) << " байт" << endl;
+    cout << "bool5 = " << bool5 << " | Размер: " << sizeof(bool5) << " байт" << endl;
+    cout << "bool6 = " << bool6 << " | Размер: " << sizeof(bool6) << " байт" << endl;
+    cout << "bool7 = " << bool7 << " | Размер: " << sizeof(bool7) << " байт" << endl;
+    cout << "bool8 = " << bool8 << " | Размер: " << sizeof(bool8) << " байт" << endl;
+    cout << "bool9 = " << bool9 << " | Размер: " << sizeof(bool9) << " байт" << endl;
     return 0;
 }
