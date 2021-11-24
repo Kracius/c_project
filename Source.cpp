835931c7b988fe50f833d545ddf304ef7a65ce17
Kracius <89945182+Kracius@users.noreply.github.com>
2021-11-24 21:12:18 +0300

    Delete Source.cpp

--- Source.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    //Скриншот. https://cdn.discordapp.com/attachments/890216273445810186/890217188332535889/unknown.png
-    return 0;
-}
